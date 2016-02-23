97501d939b52a281e62161bfb7f40e9921dd4fd1
eben <ebenewood@yahoo.com>
2016-02-22 21:57:17 -0800

    creates a triangle evaluation logic partly

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,42 @@
-// $(function() {
-if (document.getElementById('yearInput') != null) {
-  z = parseInt(document.getElementById("yearInput").value);
-} else {
-  z = null;
+// a = parseInt(document.getElementById('degree1').value);
+// b = parseInt(document.getElementById('degree2').value);
+// c = parseInt(document.getElementById('degree3').value);
+
+function size (a, b, c) {
+  if ((a + b + c > 180) || (a + b + c < 180)) {
+    $('div.ans').text('this is not a triangle');
+  } else if ((a == b) && (a == c) && (c == b)) {
+    $('div.ans').text('this is an equalaterial triangle');
+  } else if ((a == b) || (a == c) || (c == b))  {
+    $('div.ans').text('this is an isosceles triangle');
+  } else {
+    $('div.ans').text('this is a scalene triangle');
+  };
 };
-  function eval (x) {
-    z = parseInt(document.getElementById("yearInput").value);
-    if ((x % 400 == 0) && (x % 4 == 0)) {
-        $('div.yes').text('this is a leap year');
-      } else {
-        $('div.yes').text('this ain\'t no leap year');
-      };
-    };
-  // event.preventDefault();
+
+$(function() {
+  $('#check').click(function() {
+    size(parseInt($('input#degree1').val()), parseInt($('input#degree2').val()), parseInt($('input#degree3').val())));
+  });
+});
+
+
+// =====
+// $(function() {
+//
+//   function get () {
+//     parseInt(document.getElementById("yearInput").value);
+//   };
+//   function eval (x) {
+//     if ((x % 400 == 0) && (x % 4 == 0)) {
+//         $('div.yes').text('this is a leap year');
+//       } else {
+//         $('div.yes').text('this ain\'t no leap year');
+//       };
+//     };
+//
+//   eval(get());
+// // event.preventDefault();
 // });
 // function yearGet () {
 //   var i = document.getElementById("yearInput").value;
