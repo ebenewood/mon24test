b9fb3b22fbc2d0ab35cd22b4bff2c913e0924fa9
eben <student@epicodus.com>
2016-02-24 19:12:06 -0800

    untangles js from functions

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,52 @@
 // b = parseInt(document.getElementById('degree2').value);
 // c = parseInt(document.getElementById('degree3').value);
 
+// var imgScal = "<img src='img/scal.jpg'>"
+// var imgIsos = "<img src='img/isos.jpg'>"
+// var imgEqua = "<img src='img/equa.jpg'>"
+// var imgEqua = "<img src='img/not.jpg'>"
+
+var ansArray = [
+  'this is not a triangle',
+  'this is an equalaterial triangle',
+  'this is an isosceles triangle',
+  'this is a scalene triangle'
+  ];
+
 function size (a, b, c) {
   if ((a + b + c > 180) || (a + b + c < 180)) {
-    $('div.ans').text('this is not a triangle');
+    ans = ansArray[0];
+    return ans;
   } else if ((a == b) && (a == c) && (c == b)) {
-    $('div.ans').text('this is an equalaterial triangle');
+    ans = ansArray[1];
+    return ans;
   } else if ((a == b) || (a == c) || (c == b))  {
-    $('div.ans').text('this is an isosceles triangle');
+    ans = ansArray[2];
+    return ans;
   } else {
-    $('div.ans').text('this is a scalene triangle');
+    ans = ansArray[3];
+    return ans;
   };
 };
 
+// function size (a, b, c) {
+//   if ((a + b + c > 180) || (a + b + c < 180)) {
+//     $('div.ans').text('this is not a triangle');
+//   } else if ((a == b) && (a == c) && (c == b)) {
+//     $('div.ans').text('this is an equalaterial triangle');
+//   } else if ((a == b) || (a == c) || (c == b))  {
+//     $('div.ans').text('this is an isosceles triangle');
+//     $('')
+//   } else {
+//     $('div.ans').text('this is a scalene triangle');
+//     $('div.img').append(imgScal);
+//   };
+// };
+
 $(function() {
   $('#check').click(function() {
-    size(parseInt($('input#degree1').val()), parseInt($('input#degree2').val()), parseInt($('input#degree3').val())));
+    size(parseInt($('input#degree1').val()), parseInt($('input#degree2').val()), parseInt($('input#degree3').val()));
+    $('div.ans').text(ans);
   });
 });
 
